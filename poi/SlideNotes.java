5ea46b951d9765426e4fedf0f3fc7074e7dcbf05
sawade <konrad.sawade@gmx.de>
2015-06-12 13:24:13 +0200

    add example for slidenotes using apache poi

---
+++ poi/SlideNotes.java
@@ -0,0 +1,44 @@
+import org.apache.poi.xslf.usermodel.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Example to add notes to a slide using Apache POI (XSLF).
+ *
+ * @author spilymp
+ */
+public class SlideNotes {
+
+    private static final String path = System.getProperty("user.home") + "/documents/";
+
+    public void addNoteToPPTX() {
+
+        // create a new empty slide show
+        XMLSlideShow pptx = new XMLSlideShow();
+
+        // add first slide
+        XSLFSlide slide = pptx.createSlide();
+
+        // get or create notes
+        XSLFNotes note = pptx.getNotesSlide(slide);
+
+        // insert text
+        for (XSLFTextShape shape : note.getPlaceholders()) {
+            if (shape.getTextType() == Placeholder.BODY) {
+                shape.setText("Lorem ipsum dolor sit ...");
+                break;
+            }
+        }
+
+        // save file
+        try {
+            OutputStream os = new FileOutputStream(path + "SlideNotes.pptx");
+            pptx.write(os);
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
